5da19361d040ce3efe145fe360f9c12874f67991
Omar Adil <40440628+omarthe95@users.noreply.github.com>
2020-05-31 04:47:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='IPregex',
 version='0.1',
 description='Validating and classifying IPv4 address ',
-url='#',
+url='https://github.com/omarthe95/IPregex',
 author='Omar Adil',
 author_email='xomaradilxa@gmail.com',
 license='MIT',
