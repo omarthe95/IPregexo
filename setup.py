3c5c18fd26a79a675ab25afe68426913aec1f635
Omar Adil <40440628+omarthe95@users.noreply.github.com>
2020-05-31 04:42:51 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='IPregex',
+version='0.1',
+description='Validating and classifying IPv4 address ',
+url='#',
+author='Omar Adil',
+author_email='xomaradilxa@gmail.com',
+license='MIT',
+packages=['IPregex'],
+zip_safe=False)
