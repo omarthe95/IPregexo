5ad2997a98edc2ee6d7ad82a4f8cca06e03046f7
Omar Adil <40440628+omarthe95@users.noreply.github.com>
2020-05-31 06:52:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="IPregex", 
+    name="IPregexo", 
     version="0.0.1",
     author="Omar Adil",
     author_email="xomaradilxa@gmail.com",
